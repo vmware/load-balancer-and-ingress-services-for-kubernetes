9582ec2223ab52d41f8378d5eccb0ed9a98ab850
Sudipta Biswas <43565730+sudswasavi@users.noreply.github.com>
2020-06-04 16:32:49 +0530

    AV-86538: Changes to copyrights and other logistics for opensource (#115)
    * AV-86538: Changes to copyrights and other logistics for opensource
    
    This commit takes care of the opensource logistics required for AKO.
    
    Summary of changes:
    
     - Corrected the copyright statements on the files.
     - Added a CODE_OF_CONDUCT.md file.
     - Moved the jenkins/scripts under hack/
     - Added a apache 2.0 LICENSE file.
    
    Pending add:
    
     - CONTRIBUTING.md
    
    * Moved the jenkins folder back to tests folder, till changes are made

--- pkg/lib/cni.go
+++ pkg/lib/cni.go
@@ -1,5 +1,5 @@
 /*
- * [2013] - [2018] Avi Networks Incorporated
+ * Copyright 2019-2020 VMware, Inc.
  * All Rights Reserved.
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-
 package lib
 
 import (
