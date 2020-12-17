4e8d4d26265505e8f5dbdf9fc16c32cd2c8535f7
shubhamavi <shubham@avinetworks.com>
2020-12-17 14:58:47 +0530

    enable ingress class workflow onlly for networking/v1 IngressClass enableed cluster k8s-1.19+

--- pkg/utils/ingress.go
+++ pkg/utils/ingress.go
@@ -30,7 +30,8 @@ func SetIngressClassEnabled(kc kubernetes.Interface) {
 
 	var isPresent bool
 	timeout := int64(120)
-	_, ingClassError := kc.NetworkingV1beta1().IngressClasses().List(context.TODO(), metav1.ListOptions{TimeoutSeconds: &timeout})
+	// should only work for k8s 1.19+ clusters
+	_, ingClassError := kc.NetworkingV1().IngressClasses().List(context.TODO(), metav1.ListOptions{TimeoutSeconds: &timeout})
 	if ingClassError != nil {
 		AviLog.Infof("networking.k8s.io/v1/IngressClass not found/enabled on cluster: %v", ingClassError)
 		isPresent = false
