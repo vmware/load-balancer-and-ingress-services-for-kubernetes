e44b3b6301c9a796175a717021aa53c45f2cb453
Sudipta Biswas <sudipto@avinetworks.com>
2021-02-03 12:47:16 +0530

    Fix nsxalbsetting CRD (#260)
    This makes two fixes:
     - Removes `network` being mandatory field.
     - Corrects the schema for the `network` section of the CRD
     - Fixes a permission in clusterrole for the CRD

--- tests/oshiftroutetests/oshift_crd_test.go
+++ tests/oshiftroutetests/oshift_crd_test.go
@@ -241,7 +241,7 @@ func TestOshiftMultiRouteToSecureHostRule(t *testing.T) {
 			}
 		}
 		return false
-	}, 30*time.Second).Should(gomega.Equal(true))
+	}, 60*time.Second).Should(gomega.Equal(true))
 	_, aviModel := objects.SharedAviGraphLister().Get(modelName)
 	nodes := aviModel.(*avinodes.AviObjectGraph).GetAviVS()
 	g.Expect(nodes[0].SniNodes[0].SSLKeyCertAviRef).To(gomega.ContainSubstring("thisisahostruleref-sslkey"))
