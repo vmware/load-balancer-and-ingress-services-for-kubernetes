79009dce7b7cd8169d6e5a80ff14154e9eb3e293
Monotosh Das <monotosh.das@avinetworks.com>
2021-04-27 09:58:32 +0530

    Use correct index while deleting Route status (#401)
    * Use correct index while deleting Route status
    
    Fixes a panic error - index out of range sometimes while updating route status
    
    * Don't try to delete VS annotation from Ingress or Route object if the annotation is not found
    
    This leads to unnecessary retry while delays bootup

--- internal/status/ing_status.go
+++ internal/status/ing_status.go
@@ -409,7 +409,8 @@ func deleteIngressAnnotation(ingObj *networkingv1beta1.Ingress, svcMeta avicache
 			return fmt.Errorf("error in unmarshalling annotations for ingress: %v", err)
 		}
 	} else {
-		return fmt.Errorf("error in fetching annotations for ingress %s/%s", ingObj.Namespace, ingObj.Name)
+		utils.AviLog.Debugf("VS annotations not found for ingress %s/%s", ingObj.Namespace, ingObj.Name)
+		return nil
 	}
 
 	for k := range existingAnnotations {
