7aa85b3ede41692c2732ec0325f35d0b0947d214
Swathin Sankaran <swathins@vmware.com>
2021-10-26 18:47:21 +0530

    Reverts the code changes to set controller version per session as the caller function already doing it

--- pkg/utils/avi_rest_utils.go
+++ pkg/utils/avi_rest_utils.go
@@ -131,10 +131,6 @@ func NewAviRestClientPool(num uint32, api_ep string, username string,
 		version, err := clientPool.AviClient[0].AviSession.GetControllerVersion()
 		if err == nil {
 			AviLog.Infof("Setting the client version to the current controller version %v", version)
-			SetVersion := session.SetVersion(version)
-			for i := uint32(0); i < num; i++ {
-				SetVersion(clientPool.AviClient[i].AviSession)
-			}
 			CtrlVersion = version
 		}
 	}
