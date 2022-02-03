3fe8aef96eb0bf2034f8b87822cb191e9effce74
RiyaGaniga-avi <riyag@vmware.com>
2022-02-03 21:10:42 +0530

    AKO Operator 1.6.2 updated documentation

--- ako-operator/controllers/crd.go
+++ ako-operator/controllers/crd.go
@@ -162,7 +162,7 @@ func createHostRuleCRD(clientset *apiextension.ApiextensionsV1Client, log logr.L
 													"enabled": {
 														Type: "boolean",
 														Default: &apiextensionv1.JSON{
-															Raw: []byte("\"false\""),
+															Raw: []byte("false"),
 														},
 													},
 													"throttle": {
@@ -190,7 +190,7 @@ func createHostRuleCRD(clientset *apiextension.ApiextensionsV1Client, log logr.L
 											"logAllHeaders": {
 												Type: "boolean",
 												Default: &apiextensionv1.JSON{
-													Raw: []byte("\"false\""),
+													Raw: []byte("false"),
 												},
 											},
 										},
