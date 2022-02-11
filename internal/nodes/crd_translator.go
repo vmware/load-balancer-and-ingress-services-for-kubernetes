cc6568087455dc404654e898dc3adf1c57c09199
Swathin Sankaran <swathins@vmware.com>
2022-02-11 15:25:06 +0530

    Sets the non significant log duration as infinite by default (#773)

--- internal/nodes/crd_translator.go
+++ internal/nodes/crd_translator.go
@@ -144,8 +144,10 @@ func BuildL7HostRule(host, key string, vsNode AviVsEvhSniModel) {
 		}
 
 		if hostrule.Spec.VirtualHost.AnalyticsPolicy != nil {
+			var infinite int32 = 0 // Special value to set log duration as infinite
 			analyticsPolicy = &models.AnalyticsPolicy{
 				FullClientLogs: &models.FullClientLogs{
+					Duration: &infinite,
 					Enabled:  hostrule.Spec.VirtualHost.AnalyticsPolicy.FullClientLogs.Enabled,
 					Throttle: lib.GetThrottle(hostrule.Spec.VirtualHost.AnalyticsPolicy.FullClientLogs.Throttle),
 				},
