787ad992e3c84c5528d2349ee27913ae478d0e87
Shubham Chauhan <shchauhan@vmware.com>
2022-05-02 17:48:20 +0530

    lint issues fixed

--- tests/npltests/npl_pod_test.go
+++ tests/npltests/npl_pod_test.go
@@ -225,7 +225,7 @@ func TestMain(m *testing.M) {
 	waitGroupMap["status"] = wgStatus
 
 	integrationtest.AddConfigMap(KubeClient)
-	ctrl.SetSEGroupCloudName()
+	ctrl.SetSEGroupCloudNameFromNSAnnotations()
 
 	integrationtest.PollForSyncStart(ctrl, 10)
 
