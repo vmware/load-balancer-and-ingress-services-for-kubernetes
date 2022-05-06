c783ba0023fa5d8ac392a053750a8afb6d7d03f3
Shubham Chauhan <shchauhan@vmware.com>
2022-05-06 19:47:12 +0530

    Add Ingress handling for VCF cluster/WCP with NSX
    Misc cleanup

--- cmd/ako-main/main.go
+++ cmd/ako-main/main.go
@@ -99,7 +99,7 @@ func InitializeAKC() {
 	var advl4Client *advl4.Clientset
 	var svcAPIClient *svcapi.Clientset
 	var istioClient *istiocrd.Clientset
-	if lib.GetAdvancedL4() {
+	if lib.IsWCP() {
 		advl4Client, err = advl4.NewForConfig(cfg)
 		if err != nil {
 			utils.AviLog.Fatalf("Error building service-api v1alpha1pre1 clientset: %s", err.Error())
@@ -177,10 +177,16 @@ func InitializeAKC() {
 	if lib.GetNamespaceToSync() != "" {
 		informersArg[utils.INFORMERS_NAMESPACE] = lib.GetNamespaceToSync()
 	}
-	informersArg[utils.INFORMERS_ADVANCED_L4] = lib.GetAdvancedL4()
+
+	// Namespace bound Secret informers should be initialized for AKO in VDS,
+	// For AKO in VCF, we will need to watch on Secrets across all namespaces.
+	if !utils.IsVCFCluster() && lib.GetAdvancedL4() {
+		informersArg[utils.INFORMERS_ADVANCED_L4] = true
+	}
+
 	utils.NewInformers(utils.KubeClientIntf{ClientSet: kubeClient}, registeredInformers, informersArg)
 	lib.NewDynamicInformers(dynamicClient, false)
-	if lib.GetAdvancedL4() {
+	if lib.IsWCP() {
 		k8s.NewAdvL4Informers(advl4Client)
 	} else {
 		k8s.NewCRDInformers(crdClient)
@@ -188,7 +194,8 @@ func InitializeAKC() {
 			k8s.NewSvcApiInformers(svcAPIClient)
 		}
 	}
-	// Set Istio Informers
+
+	// Set Istio Informers.
 	if lib.IsIstioEnabled() {
 		k8s.NewIstioCRDInformers(istioClient)
 	}
