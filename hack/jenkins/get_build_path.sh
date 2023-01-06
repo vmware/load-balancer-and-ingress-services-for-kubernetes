d94c43358657d0397ca989e6c844c1b7bc85595a
Aakash Kumar Dixit <aakashd1@vmware.com>
2023-01-06 12:47:24 +0000

    AV-166170 : Adding script to get build path for SRP data submission

---
+++ hack/jenkins/get_build_path.sh
@@ -0,0 +1,40 @@
+# Copyright 2019-2023 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+set -xe
+
+if [ $# -lt 2 ] ; then
+    echo "Usage: ./get_build_path.sh <branch> <build_num>";
+    exit 1
+fi
+
+BRANCH=$1
+BUILD_NUMBER=$2
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Function to get GIT workspace root location
+function get_git_ws {
+    git_ws=$(git rev-parse --show-toplevel)
+    [ -z "$git_ws" ] && echo "Couldn't find git workspace root" && exit 1
+    echo $git_ws
+}
+
+BUILD_VERSION_SCRIPT=$SCRIPTPATH/get_build_version.sh
+build_version=$(bash $BUILD_VERSION_SCRIPT "dummy" $BUILD_NUMBER)
+
+target_path=/mnt/builds/ako_OS/$BRANCH/ci-build-$build_version
+echo $target_path
