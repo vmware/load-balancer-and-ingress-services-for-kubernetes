2c64ea09a7ddcbe68c74df9d5b984ee077bb93c1
Arihant Bachhawat <barihant@vmware.com>
2023-03-21 04:46:50 +0000

    update max version of avi controller

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -97,7 +97,7 @@ const (
 	CTRL_VERSION_22_1_3           = "22.1.3"
 	CTRL_VERSION_22_1_2           = "22.1.2"
 	Namespace                     = "Namespace"
-	MaxAviVersion                 = "22.1.2"
+	MaxAviVersion                 = "22.1.3"
 
 	RefreshAuthTokenInterval = 12  //hours
 	AuthTokenExpiry          = 240 //hours
