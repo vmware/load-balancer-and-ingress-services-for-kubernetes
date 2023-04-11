40786f2b2ed2e8282aff078371f692074720c259
Swathin Sankaran <swathins@vmware.com>
2023-04-12 00:39:47 +0530

    This commit contains the ingestion layer code changes for L4 CRD: (#1091)
      1. Ingestion layer code changes for L4 CRD.
      2. L4 CRD validations.
      3. Helm chart changes required for L4 CRD.

--- internal/lib/control_config.go
+++ internal/lib/control_config.go
@@ -35,6 +35,8 @@ import (
 
 	akocrd "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha1/clientset/versioned"
 	akoinformer "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha1/informers/externalversions/ako/v1alpha1"
+	v1alpha2akocrd "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha2/clientset/versioned"
+	v1alpha2akoinformer "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha2/informers/externalversions/ako/v1alpha2"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/utils"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/third_party/github.com/vmware/alb-sdk/go/clients"
 
@@ -56,6 +58,7 @@ type AKOCrdInformers struct {
 	HostRuleInformer        akoinformer.HostRuleInformer
 	HTTPRuleInformer        akoinformer.HTTPRuleInformer
 	AviInfraSettingInformer akoinformer.AviInfraSettingInformer
+	L4RuleInformer          v1alpha2akoinformer.L4RuleInformer
 }
 
 type IstioCRDInformers struct {
@@ -107,6 +110,13 @@ type akoControlConfig struct {
 	// httpRuleEnabled is set to true if the cluster has
 	// HTTPRule CRD installed.
 	httpRuleEnabled bool
+
+	// client-set and informer for v1alpha2 of AKO CRD.
+	v1alpha2crdClientset v1alpha2akocrd.Interface
+
+	// l4RuleEnabled is set to true if the cluster has
+	// L4Rule CRD installed.
+	l4RuleEnabled bool
 
 	// licenseType holds the default license tier which would be used by new Clouds. Enum options - ENTERPRISE_16, ENTERPRISE, ENTERPRISE_18, BASIC, ESSENTIALS.
 	licenseType string
@@ -214,6 +224,15 @@ func (c *akoControlConfig) CRDClientset() akocrd.Interface {
 	return c.crdClientset
 }
 
+func (c *akoControlConfig) Setv1alpha2CRDClientset(cs v1alpha2akocrd.Interface) {
+	c.v1alpha2crdClientset = cs
+	c.Setv1alpha2CRDEnabledParams(cs)
+}
+
+func (c *akoControlConfig) V1alpha2CRDClientset() v1alpha2akocrd.Interface {
+	return c.v1alpha2crdClientset
+}
+
 func (c *akoControlConfig) SetCRDEnabledParams(cs akocrd.Interface) {
 	timeout := int64(120)
 	_, aviInfraError := cs.AkoV1alpha1().AviInfraSettings().List(context.TODO(), metav1.ListOptions{TimeoutSeconds: &timeout})
@@ -244,6 +263,18 @@ func (c *akoControlConfig) SetCRDEnabledParams(cs akocrd.Interface) {
 	}
 }
 
+func (c *akoControlConfig) Setv1alpha2CRDEnabledParams(cs v1alpha2akocrd.Interface) {
+	timeout := int64(120)
+	_, err := cs.AkoV1alpha2().L4Rules(metav1.NamespaceAll).List(context.TODO(), metav1.ListOptions{TimeoutSeconds: &timeout})
+	if err != nil {
+		utils.AviLog.Infof("ako.vmware.com/v1alpha2/L4Rule not found/enabled on cluster: %v", err)
+		c.l4RuleEnabled = false
+	} else {
+		utils.AviLog.Infof("ako.vmware.com/v1alpha2/L4Rule enabled on cluster")
+		c.l4RuleEnabled = true
+	}
+}
+
 func (c *akoControlConfig) AviInfraSettingEnabled() bool {
 	return c.aviInfraSettingEnabled
 }
@@ -254,6 +285,10 @@ func (c *akoControlConfig) HostRuleEnabled() bool {
 
 func (c *akoControlConfig) HttpRuleEnabled() bool {
 	return c.httpRuleEnabled
+}
+
+func (c *akoControlConfig) L4RuleEnabled() bool {
+	return c.l4RuleEnabled
 }
 
 func (c *akoControlConfig) ControllerVersion() string {
