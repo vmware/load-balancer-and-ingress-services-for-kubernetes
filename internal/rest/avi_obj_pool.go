8614ac962d324b80d33edc82fb367315c01f335e
Swathin Sankaran <swathins@vmware.com>
2023-04-19 18:22:44 +0530

    [L4 CRD] Rest layer Changes  (#1106)
    * This commit adds the rest layer changes for L4 CRD.
    
    * Minor fixes

--- internal/rest/avi_obj_pool.go
+++ internal/rest/avi_obj_pool.go
@@ -23,6 +23,7 @@ import (
 
 	"google.golang.org/protobuf/proto"
 
+	"github.com/jinzhu/copier"
 	avicache "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/cache"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/lib"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/nodes"
--- internal/rest/avi_obj_vs.go
+++ internal/rest/avi_obj_vs.go
@@ -22,6 +22,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/jinzhu/copier"
 	avicache "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/cache"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/lib"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/nodes"
