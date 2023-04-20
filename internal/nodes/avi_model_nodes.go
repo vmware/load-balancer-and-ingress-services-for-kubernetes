ba2f60ce3fbd1ad894c1db1ff4555035b3302535
Swathin Sankaran <swathins@vmware.com>
2023-04-20 11:23:27 +0530

    Minor fixes

--- internal/nodes/avi_model_nodes.go
+++ internal/nodes/avi_model_nodes.go
@@ -25,7 +25,6 @@ import (
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/lib"
 	akov1alpha1 "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/apis/ako/v1alpha1"
 	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/utils"
-	"google.golang.org/protobuf/proto"
 
 	avimodels "github.com/vmware/alb-sdk/go/models"
 	"google.golang.org/protobuf/proto"
