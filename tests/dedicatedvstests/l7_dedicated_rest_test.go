c56e00ef205dd5b9e02238b50a20fff359e64064
Swathin Sankaran <swathins@vmware.com>
2023-06-07 12:29:24 +0530

    AV-180638: Fix for the network profile for SCTP not coming as expected (#1166)
    * Fix for the network profile for SCTP not coming as expected
    
    This commit fixes one regression introduced as part of the L4Rule
    and SSORule CRD code merge. The function `copier.Copy` that copies
    the value by using the field names was overwriting the application
    profile and network profile to an empty string. Because of this
    issue, the application profile and network profile was getting
    configured with their default values only every time.
    
    * This commit adds new UTs to verify the application and network profile
    attached to a evh, sni parent and child vs and dedicated vs.
    
    * This commit fixes few UT issues.

---
+++ tests/dedicatedvstests/l7_dedicated_rest_test.go
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023-2024 VMware, Inc.
+ * All Rights Reserved.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*   http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package dedicatedvstests
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/onsi/gomega"
+
+	avinodes "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/nodes"
+	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/internal/objects"
+	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/tests/integrationtest"
+)
+
+func TestProfilesAttachedToDedicatedSecureVS(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	// middleware verifies the application and network profiles attached to the VS
+	integrationtest.AddMiddleware(func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL.EscapedPath()
+		if r.Method == http.MethodPost &&
+			strings.Contains(url, "/api/virtualservice") {
+			var resp map[string]interface{}
+			data, _ := io.ReadAll(r.Body)
+			json.Unmarshal(data, &resp)
+			g.Expect(resp["application_profile_ref"]).Should(gomega.HaveSuffix("System-Secure-HTTP"))
+			g.Expect(resp["network_profile_ref"]).Should(gomega.HaveSuffix("System-TCP-Proxy"))
+			return
+		}
+		integrationtest.NormalControllerServer(w, r)
+	})
+
+	modelName := "admin/cluster--foo.com-L7-dedicated"
+	SetUpIngressForCacheSyncCheck(t, true, true, modelName)
+	g.Eventually(func() int {
+		_, aviModel := objects.SharedAviGraphLister().Get(modelName)
+		nodes, ok := aviModel.(*avinodes.AviObjectGraph)
+		if !ok {
+			return 0
+		}
+		return len(nodes.GetAviVS())
+	}, 30*time.Second).Should(gomega.Equal(1))
+
+	TearDownIngressForCacheSyncCheck(t, modelName)
+
+	integrationtest.ResetMiddleware()
+
+}
