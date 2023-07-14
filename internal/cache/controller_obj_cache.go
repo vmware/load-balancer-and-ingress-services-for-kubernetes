1e360213d6a4a1acd5b4d4ea11046bd9c50088fb
Swathin Sankaran <swathins@vmware.com>
2023-07-14 19:00:24 +0530

    Code cleanup and a few bug fixes (#1201)
    This commit changes the following:
       1. Adds a Makefile target to compile the AKO's Gateway API
          code.
       2. Cleanup of the code.
       3. Fixes a few bugs present in the Gateway API code.
       4. Adds the missing environment variables to the statefulset under
          the Gateway API container.

--- internal/cache/controller_obj_cache.go
+++ internal/cache/controller_obj_cache.go
@@ -2806,9 +2806,9 @@ func checkRequiredValuesYaml(client *clients.AviClient, returnErr *error) bool {
 		return false
 	}
 
-	lib.SetNamePrefix()
+	lib.SetNamePrefix("")
 	// after clusterName validation, set AKO User to be used in created_by fields for Avi Objects
-	lib.SetAKOUser("ako-")
+	lib.SetAKOUser(lib.AKOPrefix)
 	//Set clusterlabel checksum
 	lib.SetClusterLabelChecksum()
 
