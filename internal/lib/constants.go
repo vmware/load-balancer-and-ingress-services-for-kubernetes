31828e81364a7787335e1bf071a727f7659b8707
abishekb <abishekb@vmware.com>
2023-07-14 11:55:11 +0530

    Add Rest Op to Slow retry if Network not found in Avi

--- internal/lib/constants.go
+++ internal/lib/constants.go
@@ -197,6 +197,7 @@ const (
 	VrfContextNotFoundError                    = "VrfContext not found"
 	HTTPMethodGet                              = "GET"
 	HTTPMethodPut                              = "PUT"
+	NetworkNotFoundError                       = "Network object not found"
 
 	// AKO Event constants
 	AKOEventComponent        = "avi-kubernetes-operator"
