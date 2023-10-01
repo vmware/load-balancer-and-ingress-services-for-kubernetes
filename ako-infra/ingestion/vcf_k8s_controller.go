5714754296fb56f4b099b8bf7c4a25df0e7a7dbd
abishekb <abishekb@vmware.com>
2023-10-01 21:09:17 +0530

    Set VPC mode to true by default

--- ako-infra/ingestion/vcf_k8s_controller.go
+++ ako-infra/ingestion/vcf_k8s_controller.go
@@ -291,7 +291,8 @@ func (c *VCFK8sController) ValidBootStrapData() bool {
 		return false
 	}
 
-	lib.SetVPCMode(configmap.Data["vpcMode"])
+	//lib.SetVPCMode(configmap.Data["vpcMode"])
+	lib.SetVPCMode("true")
 	lib.SetClusterID(clusterID)
 	setTranzportZone(transportzone)
 	return c.ValidBootstrapSecretData(controllerIP, secretName, secretNamespace)
