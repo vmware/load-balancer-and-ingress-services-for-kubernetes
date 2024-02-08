03349fe19c75cc8b8943a3592acef976eaf521ef
jeyapradeen-avi <jpradeen@vmware.com>
2024-02-08 10:28:33 +0000

    AV-191652: Jenkins file conversion changes

---
+++ ako_cicd/jenkins/common-scripts/generateGitChangeLogReport.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe 
+
+REPORT_FILE=${REPORT_FILE:-"GitChangeLogReport.html"}
+
+# Delete old report file/s
+rm -rf $WORKSPACE/ako_cicd/jenkins/git-changelog/$REPORT_FILE
+rm -rf $WORKSPACE/$REPORT_FILE
+
+cd $WORKSPACE/ako_cicd/jenkins/git-changelog;
+
+# Extract, print, and generate report file for GIT change logs
+python gitChangeLogExtractor.py CI smoke $BUILD_NUMBER -r $REPORT_FILE --file-mode
+
+# If previous script failed, at least touch an empty report file to prevent post-build action failure
+if [ ! -f ./$REPORT_FILE ]; 
+then
+    echo "Git ChangeLog Report file not found. Creating an empty report file"
+    touch ./$REPORT_FILE
+fi
+
+cp ./$REPORT_FILE $WORKSPACE/;
+
