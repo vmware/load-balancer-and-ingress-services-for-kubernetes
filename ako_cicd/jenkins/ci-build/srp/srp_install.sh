152bfb06aee90e8fdfc0790de9029ca7cc58aa1b
Jeya Pradeen <jeya.pradeen@broadcom.com>
2024-12-10 09:32:08 +0000

    Removed port number

--- ako_cicd/jenkins/ci-build/srp/srp_install.sh
+++ ako_cicd/jenkins/ci-build/srp/srp_install.sh
@@ -4,12 +4,12 @@ set -xe
 #SRP intallation
 sudo rm -rf /srp-tools
 sudo mkdir /srp-tools
-sudo wget --tries=5 --timeout=60 --quiet --output-document /srp-tools/srp https://packages.vcfd.broadcom.net:443/artifactory/srp-generic-local/srpcli/1.5.1-20241206185708-af7c503/linux-amd64/srp
+sudo wget --tries=5 --timeout=60 --quiet --output-document /srp-tools/srp https://packages.vcfd.broadcom.net/artifactory/srp-generic-local/srpcli/1.5.1-20241206185708-af7c503/linux-amd64/srp
 sudo chmod +x /srp-tools/srp
 sudo /srp-tools/srp --version
 sudo /srp-tools/srp update --yes
 sudo /srp-tools/srp --version
-sudo wget --tries=5 --timeout=60 --quiet --output-document /tmp/linux-observer-4.4.0.tar.gz https://packages.vcfd.broadcom.net:443/artifactory/srp-generic-local/observer/4.4.0/linux-observer-4.4.0.tar.gz
+sudo wget --tries=5 --timeout=60 --quiet --output-document /tmp/linux-observer-4.4.0.tar.gz https://packages.vcfd.broadcom.net/artifactory/srp-generic-local/observer/4.4.0/linux-observer-4.4.0.tar.gz
 sudo mkdir /srp-tools/observer
 cd /srp-tools/observer
 sudo tar zxf /tmp/linux-observer-4.4.0.tar.gz
