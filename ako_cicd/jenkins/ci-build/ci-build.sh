381f4580ce2ae048fa0cac50580b9b04708524bc
Jeya Pradeen <jpradeen@vmware.com>
2025-01-06 13:45:53 +0530

    Update Photon image to latest 5.0

--- ako_cicd/jenkins/ci-build/ci-build.sh
+++ ako_cicd/jenkins/ci-build/ci-build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 export GOLANG_SRC_REPO=${PVT_DOCKER_REGISTRY}/golang:latest
-export PHOTON_SRC_REPO=${VMWARE_DOCKER_REGISTRY}/photon/photon4:latest
+export PHOTON_SRC_REPO=${PVT_DOCKER_REGISTRY}/photon:5.0
 
 branch_version=$($WORKSPACE/hack/jenkins/get_branch_version.sh)
 version_numbers=(${branch_version//./ })
