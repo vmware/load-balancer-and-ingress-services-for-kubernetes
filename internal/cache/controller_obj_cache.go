aba63a3967d19a7f90735ce3446c56697d7023f3
Abishek Bahadur Singh <abishek-bahadur.singh@broadcom.com>
2025-03-17 18:14:30 +0530

    Include Pool name in L4 Policy checksum calculation

--- internal/cache/controller_obj_cache.go
+++ internal/cache/controller_obj_cache.go
@@ -1613,7 +1613,7 @@ func (c *AviObjCache) AviPopulateOneVsL4PolCache(client *clients.AviClient,
 			}
 		}
 		emptyIngestionMarkers := utils.AviObjectMarkers{}
-		cksum := lib.L4PolicyChecksum(ports, protocols, emptyIngestionMarkers, l4pol.Markers, true)
+		cksum := lib.L4PolicyChecksum(ports, protocols, pools, emptyIngestionMarkers, l4pol.Markers, true)
 		tenant := getTenantFromTenantRef(*l4pol.TenantRef)
 		l4PolCacheObj := AviL4PolicyCache{
 			Name:             *l4pol.Name,
@@ -1920,7 +1920,7 @@ func (c *AviObjCache) AviPopulateAllL4PolicySets(client *clients.AviClient, clou
 		}
 
 		emptyIngestionMarkers := utils.AviObjectMarkers{}
-		cksum := lib.L4PolicyChecksum(ports, protocols, emptyIngestionMarkers, l4pol.Markers, true)
+		cksum := lib.L4PolicyChecksum(ports, protocols, pools, emptyIngestionMarkers, l4pol.Markers, true)
 		l4PolCacheObj := AviL4PolicyCache{
 			Name:             *l4pol.Name,
 			Tenant:           getTenantFromTenantRef(*l4pol.TenantRef),
