3c4ee4f8ffa8dcf220287989da5c03d8a1c4d2b8
Jeya Pradeen <jpradeen@vmware.com>
2025-04-22 19:14:20 +0530

    AV-236936: Replace local docker registry with packages.vcfd.broadcom.net docker proxy to pull latest images always

--- ako_cicd/jenkins/ci-build/ci-build.sh
+++ ako_cicd/jenkins/ci-build/ci-build.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-export GOLANG_SRC_REPO=${PVT_DOCKER_REGISTRY}/dockerhub-proxy-cache/library/golang:latest
-export PHOTON_SRC_REPO=${PVT_DOCKER_REGISTRY}/dockerhub-proxy-cache/library/photon:5.0
+export GOLANG_SRC_REPO=avi-alb-docker-virtual.packages.vcfd.broadcom.net/golang:latest
+export PHOTON_SRC_REPO=avi-alb-docker-virtual.packages.vcfd.broadcom.net/photon:5.0
 
 branch_version=$($WORKSPACE/hack/jenkins/get_branch_version.sh)
 version_numbers=(${branch_version//./ })
