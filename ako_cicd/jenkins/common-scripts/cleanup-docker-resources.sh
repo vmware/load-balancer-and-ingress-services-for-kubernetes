ad1cdf46346a3a9133a449bc4141df87fb57bb84
Jeya Pradeen <jpradeen@vmware.com>
2025-05-09 17:17:40 +0530

    Update cleanup-docker-resources.sh

--- ako_cicd/jenkins/common-scripts/cleanup-docker-resources.sh
+++ ako_cicd/jenkins/common-scripts/cleanup-docker-resources.sh
@@ -5,3 +5,9 @@ sudo docker ps -a -q | xargs -r sudo docker rm -f
 
 # Remove all unused images, containers, and volumes
 sudo docker system prune -a -f
+
+#Remove all the build cache data
+sudo docker builder prune -a -f
+
+#pring docker system usage on disk
+sudo docker system df
