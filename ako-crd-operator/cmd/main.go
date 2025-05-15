faff0ab41c3ebfa4cd237843ae639f683c2a19c7
Priya Koshta <priya.koshta@broadcom.com>
2025-05-15 06:30:46 +0000

    AV-238750 Adding Application profile CRD to ako-crd-operator

--- ako-crd-operator/cmd/main.go
+++ ako-crd-operator/cmd/main.go
@@ -107,6 +107,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "HealthMonitor")
 		os.Exit(1)
 	}
+	if err = (&controller.ApplicationProfileReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ApplicationProfile")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
