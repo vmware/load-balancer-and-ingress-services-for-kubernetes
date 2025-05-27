adf7be52902fe770613b795d007a0612b0298101
Ashish Raman Nayak <ashishn@vmware.com>
2025-05-27 20:02:33 +0530

    AV-239699: Overwrite OOB changes on reboot (#1783)
    * AV-239699: Avoid reconcile on restart
    
    * Add observedGeneration
    
    * Implement cache
    
    * Address comments
    
    * Add UTC to controller timestamp for uniformity

---
+++ ako-crd-operator/internal/cache/cache.go
@@ -0,0 +1,87 @@
+package cache
+
+import (
+	"encoding/json"
+	avisession "github.com/vmware/alb-sdk/go/session"
+	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/ako-crd-operator/internal/session"
+	"github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/utils"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type cache struct {
+	dataStore sync.Map
+	session   *session.Session
+}
+
+type CacheOperation interface {
+	PopulateCache(...string) error
+	GetObjectByUUID(string) (dataMap, bool)
+}
+
+func NewCache(session *session.Session) CacheOperation {
+	return &cache{
+		dataStore: sync.Map{},
+		session:   session}
+}
+
+type dataMap map[string]interface{}
+
+func (d dataMap) GetLastModifiedTimeStamp() time.Time {
+	timestamp, ok := d["_last_modified"]
+	if !ok {
+		return time.Unix(0, 0)
+	}
+	timeInt, _ := strconv.ParseInt(timestamp.(string), 10, 64)
+	return time.UnixMicro(timeInt).UTC()
+}
+
+func (c *cache) PopulateCache(urls ...string) error {
+	setTenant := avisession.SetTenant("*")
+	aviSession := c.session.GetAviClients().AviClient[0].AviSession
+	_ = setTenant(aviSession)
+	// TODO: get from env variable
+	setDefaultTenant := avisession.SetTenant("admin")
+	defer setDefaultTenant(aviSession)
+
+	params := avisession.SetParams(map[string]string{"fields": "_last_modified,uuid", "page_size": "100"})
+	for _, url := range urls {
+		for url != "" {
+			dataList := []map[string]interface{}{}
+			// TODO: use ako-crd-operator session object interface instead directly accessing
+			result, err := c.session.GetAviClients().AviClient[0].AviSession.GetCollectionRaw(url, params)
+			url = result.Next
+			if err != nil {
+				return err
+			}
+			if err := json.Unmarshal(result.Results, &dataList); err != nil {
+				return err
+			}
+			for _, data := range dataList {
+				UUID, ok := data["uuid"].(string)
+				if !ok {
+					utils.AviLog.Warnf("unable to find uuid in object :[%v]", data)
+					continue
+				}
+				c.dataStore.Store(UUID, data)
+			}
+		}
+	}
+	utils.AviLog.Infof("populated cache successfully for urls: [%v]", urls)
+	return nil
+}
+
+func (c *cache) GetObjectByUUID(UUID string) (dataMap, bool) {
+	data, ok := c.dataStore.Load(UUID)
+	if !ok {
+		utils.AviLog.Warnf("UUID: [%s] not found in cache", UUID)
+		return nil, false
+	}
+	dataMap, ok := data.(map[string]interface{})
+	if !ok {
+		utils.AviLog.Warnf("dataMap not type of map interface. type: %T", data)
+		return nil, false
+	}
+	return dataMap, true
+}
