69cd91990bea142fb22a9e12adb1d19763918ccc
Ashish Nayak <ashish-raman.nayak@broadcom.com>
2025-06-17 15:54:19 +0530

    Fix indent

--- ako-crd-operator/scripts/pre_stop_hook.sh
+++ ako-crd-operator/scripts/pre_stop_hook.sh
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 VMware, Inc.
+# Copyright 2019-2025 VMware, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,19 +16,19 @@ set -e
 function zip_old_files() {
 	if [ $USE_PVC ]
 	then
-	cd $LOG_FILE_PATH	
-	old_pods_gz_file=$LOG_FILE_PATH/old_logs.tar.gz
-	if [ ! -f "$old_pods_gz_file" ]
-	then
-		tar -czf $old_pods_gz_file $POD_NAME*.log*
- 	else
-        mkdir temp_dir
-		tar -xzf $old_pods_gz_file -C temp_dir
-		cp $POD_NAME*.log* temp_dir
-		tar -czf $old_pods_gz_file -C temp_dir .
-        rm -rf temp_dir
-        fi		
-    fi
+		cd $LOG_FILE_PATH	
+		old_pods_gz_file=$LOG_FILE_PATH/old_logs.tar.gz
+		if [ ! -f "$old_pods_gz_file" ]
+		then
+			tar -czf $old_pods_gz_file $POD_NAME*.log*
+		else
+			mkdir temp_dir
+			tar -xzf $old_pods_gz_file -C temp_dir
+			cp $POD_NAME*.log* temp_dir
+			tar -czf $old_pods_gz_file -C temp_dir .
+			rm -rf temp_dir
+		fi		
+	fi
 	rm -rf $POD_NAME*.log*
 }
 zip_old_files
