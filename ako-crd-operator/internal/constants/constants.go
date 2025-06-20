b2c80ec1bb86f3672177b8e640dc5c86514da069
Ashish Raman Nayak <ashish-raman.nayak@broadcom.com>
2025-06-20 14:48:11 +0530

    AV-239696: Add status condition on deletion failure  (#1811)
    * AV-239696: Add status condition on deletion failure due to object being referred to other avi objects
    
    * Address deletion for app profile
    
    * Address comments

--- ako-crd-operator/internal/constants/constants.go
+++ ako-crd-operator/internal/constants/constants.go
@@ -1,9 +1,12 @@
 package constants
 
+import "time"
+
 const (
 	HealthMonitorFinalizer      = "healthmonitor.ako.vmware.com/finalizer"
 	HealthMonitorURL            = "/api/healthmonitor"
 	Sensitive                   = "<sensitive>"
 	ApplicationProfileFinalizer = "applicationprofile.ako.vmware.com/finalizer"
 	ApplicationProfileURL       = "/api/applicationprofile"
+	RequeueInterval             = 5 * time.Minute
 )
