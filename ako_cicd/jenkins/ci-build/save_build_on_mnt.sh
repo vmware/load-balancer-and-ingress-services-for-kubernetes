381e96cc2ed5a56e5f38032870fb34795131f482
Jeya Pradeen <jpradeen@vmware.com>
2025-08-14 13:12:12 +0530

    AV-247822:  Update script to not copy ako-operator related helm files

--- ako_cicd/jenkins/ci-build/save_build_on_mnt.sh
+++ ako_cicd/jenkins/ci-build/save_build_on_mnt.sh
@@ -18,25 +18,18 @@ function get_git_ws {
 
 BUILD_VERSION_SCRIPT=$WORKSPACE/hack/jenkins/get_build_version.sh
 AKO_CHARTS_PATH="$(get_git_ws)/helm/ako"
-AKO_OPERATOR_CHARTS_PATH="$(get_git_ws)/ako-operator/helm/ako-operator"
 
 build_version=$(bash $BUILD_VERSION_SCRIPT "dummy" $BUILD_NUMBER)
 
 target_path=/mnt/builds/ako_OS/$BRANCH/ci-build-$build_version
 ako_target_path=$target_path/ako
-ako_operator_target_path=$target_path/ako-operator
 
 sudo mkdir -p $target_path
 sudo mkdir -p $ako_target_path
-sudo mkdir -p $ako_operator_target_path
 
 # Copy ako helm charts
 sudo cp -r $AKO_CHARTS_PATH/* $ako_target_path/
 sudo sed -i --regexp-extended "s/^(\s*)(appVersion\s*:\s*latest\s*$)/\1appVersion: $build_version/" $ako_target_path/Chart.yaml
-
-# Copy ako-operator helm charts
-sudo cp -r $AKO_OPERATOR_CHARTS_PATH/* $ako_operator_target_path/
-
 
 sudo cp "$(get_git_ws)/HEAD_COMMIT" $target_path/
 if [ "$?" != "0" ]; then
