b1b895b6cafc0f6be04ece1f3555a1cb9777de2e
srinivassurishetty <srinivas.surishetty@broadcom.com>
2025-09-13 19:17:00 +0000

    unit tests

--- tests/infratests/akoinfra_test.go
+++ tests/infratests/akoinfra_test.go
@@ -41,6 +41,7 @@ var gvrToKind = map[schema.GroupVersionResource]string{
 	lib.ClusterNetworkGVR:          "clusternetworkinfosList",
 	lib.VPCNetworkConfigurationGVR: "vpcnetworkconfigurationsList",
 	lib.AvailabilityZoneVR:         "availabilityzonesList",
+	lib.SupervisorCapabilityGVR:    "capabilitiesList",
 }
 
 func annotateNamespaceWithVpcNetworkConfigCR(t *testing.T, ns, vpcNetConfigCR string) {
