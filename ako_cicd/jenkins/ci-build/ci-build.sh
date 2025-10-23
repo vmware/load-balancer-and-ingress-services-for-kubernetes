3d7b3e45d7361d0a035e7afc10572ef48c8fde45
Gurunath Reddy Kambala <gurunath-reddy.kambala@broadcom.com>
2025-10-23 17:31:29 +0530

    AV-254457 remove minor_version condition for 2.x.y series (#1994)

--- ako_cicd/jenkins/ci-build/ci-build.sh
+++ ako_cicd/jenkins/ci-build/ci-build.sh
@@ -5,10 +5,6 @@ set -xe
 export GOLANG_SRC_REPO=avi-alb-docker-virtual.packages.vcfd.broadcom.net/golang:latest
 export PHOTON_SRC_REPO=photonos-docker-local.packages.vcfd.broadcom.net/photon5:latest
 
-branch_version=$($WORKSPACE/hack/jenkins/get_branch_version.sh)
-version_numbers=(${branch_version//./ })
-minor_version=${version_numbers[1]}
-
 export PATH=$PATH:/usr/local/go/bin
 go version
 
@@ -16,10 +12,9 @@ make build
 make BUILD_TAG=$version_tag docker
 make BUILD_TAG=$version_tag ako-operator-docker
 
-if [ "$minor_version" -ge "11" ]; then
-    make BUILD_TAG=$version_tag build-gateway-api
-    make BUILD_TAG=$version_tag ako-gateway-api-docker
-fi
+make BUILD_TAG=$version_tag build-gateway-api
+make BUILD_TAG=$version_tag ako-gateway-api-docker
+
 
 if [ "$RUN_TESTS" = true ]; then
     make test
