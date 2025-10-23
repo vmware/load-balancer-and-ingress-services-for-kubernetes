4295e54c8d9c851a4b29e76eae2115f044359722
Gurunath Reddy Kambala <gurunath-reddy.kambala@broadcom.com>
2025-10-23 17:31:29 +0530

    AV-254457 remove minor_version condition for 2.x.y series (#1994)

--- ako_cicd/jenkins/ci-build/save_build_on_registry.sh
+++ ako_cicd/jenkins/ci-build/save_build_on_registry.sh
@@ -5,15 +5,10 @@ set -xe
 sudo docker images
 
 version_tag=$($WORKSPACE/hack/jenkins/get_build_version.sh $JOB_NAME $BUILD_NUMBER)
-branch_version=$($WORKSPACE/hack/jenkins/get_branch_version.sh)
-version_numbers=(${branch_version//./ })
-minor_version=${version_numbers[1]}
 
 AKO_IMAGES=("ako" "ako-operator" "ako-crd-operator")
 
-if [ "$minor_version" -ge "11" ]; then
-    AKO_IMAGES+=("ako-gateway-api")
-fi
+AKO_IMAGES+=("ako-gateway-api")
 
 echo ${AKO_IMAGES[@]}
 
