cc13993ca64aeaba04e9276bd46ef89ef125682b
gurunath-avi <gurunath-reddy.kambala@broadcom.com>
2025-10-23 10:50:45 +0000

    AV-254457 remove minor_version condition for 2.x.y series

--- ako_cicd/jenkins/ci-build/sync-image.sh
+++ ako_cicd/jenkins/ci-build/sync-image.sh
@@ -9,7 +9,6 @@ if [ "${#registries[@]}" == "0" ]; then
     exit 1
 fi
 
-#branch_version=$($WORKSPACE/hack/jenkins/get_branch_version.sh)
 version_tag=$($WORKSPACE/hack/jenkins/get_build_version.sh $JOB_NAME $build_num)
 
 
@@ -38,8 +37,6 @@ fi
 
 ###########
 AKO_IMAGES=("ako" "ako-operator" "ako-crd-operator")
-# version_numbers=(${branch_version//./ })
-# minor_version=${version_numbers[1]}
 
 AKO_IMAGES+=("ako-gateway-api")
 
